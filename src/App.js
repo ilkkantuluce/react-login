f5a9413d9466930947c7db4e7998d80a4ff352c4
ilkkantuluce <ilkkantuluce07@gamil.com>
2023-05-20 02:10:52 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import { useGoogleLogin } from '@react-oauth/google';
+import { googleLogout } from '@react-oauth/google';
+import axios from "axios"
 
 function App() {
+
+  const [profile, setProfile] = useState(null);
+
+  const login = useGoogleLogin({
+    onSuccess: async respose => {
+      try {
+        const res = await axios.get("https://www.googleapis.com/oauth2/v3/userinfo", {
+          headers: {
+            "Authorization": `Bearer ${respose.access_token}`
+          }
+        })
+        setProfile(res.data)
+        //console.log(res.data)
+      } catch (err) {
+        console.log(err)
+      }
+    }
+  });
+
+  const logOut = () => {
+    setProfile(null);
+    googleLogout();
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {
+          profile ? (
+            <div>
+              <img src={profile.picture} alt="user image" />
+              <h3>Gebruiker ingelogd</h3>
+              <p>Naam: {profile.name}</p>
+              <p>Email: {profile.email}</p>
+              <p>Gevestigde land: {profile.locale}</p>
+              <button onClick={logOut}>Uitloggen</button>
+            </div>
+          ) : (
+            <button onClick={() => login()}>Log in met Google</button>
+          )}
+
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <GoogleOAuthProvider clientId="698917628220-p8eb9ro47uidlkfgdum4gpfqk2h1hfnr.apps.googleusercontent.com">
+      <App />
+    </GoogleOAuthProvider>;
+
   </React.StrictMode>
 );
 
